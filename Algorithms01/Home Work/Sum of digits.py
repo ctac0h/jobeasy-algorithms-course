7fb56f26d10d0991d9c7da3d4d3b8ea338f88925
Stan Mamontov <compizzz@gmail.com>
2020-06-13 19:50:07 -0700

    Algorithms 01 Home work fix

--- Algorithms01/Home Work/Sum of digits.py
+++ Algorithms01/Home Work/Sum of digits.py
@@ -1,5 +1,6 @@
 from random import randint
 
+
 def sum_of_digits_string(num):
     if num < 0:
         return None
@@ -19,17 +20,30 @@ def sum_of_digits_no_string(num):
     return result
 
 
+def get_random_int_with_n_digits_v1(n):
+    return randint(10 ** (n-1), 10 ** n-1)
+
+
+def get_random_int_with_n_digits_v2(n):
+    return int("".join([str(randint(0,10)) for j in range(n)]))
+
+
 # Test
-print()
-print("Homework test")
-print()
+print("\nHomework test\n")
+
+digits = int(input('Enter a number of digits: '))
+tests = int(input('Enter number of tests: '))
+if digits < 1:
+    digits = 1
+if tests < 1:
+    tests = 1
 
 
-for i in range(5):
-    rand_num = randint(0, 99999999)
-    print('sum_of_digits_string')
+for i in range(tests):
+    rand_num = get_random_int_with_n_digits_v1(digits)
+    print('\nsum_of_digits_string')
     print(f'Random number is: {rand_num}\n'
           f'The sum of it\'s digits is: {sum_of_digits_string(rand_num)}')
     print('sum_of_digits_no_string')
     print(f'Random number is: {rand_num}\n'
-          f'The sum of it\'s digits is: {sum_of_digits_no_string(rand_num)}\n')
+          f'The sum of it\'s digits is: {sum_of_digits_no_string(rand_num)}')
